edc7eda396190d7fe4d8c2b7d51c8a880cec1c57
E1404H5K <51128442+E1404H5K@users.noreply.github.com>
2019-06-02 00:02:20 +0700

    Add files via upload

---
+++ quiz.py
@@ -0,0 +1,345 @@
+print("""
+\033[1;91m •________________________•
+ \**********************/
+  \ HumanScriptKiddie  /
+  <....................>
+  ||||||||||||||||||||||
+  \   (¤)        (¤)   /
+   \       /  \       /
+    \      \••/      /
+•========================•
+***AUTHOR : E1404HSK******
+****CONTACT : 6282119438921**
+Test Bahasa pemograman 
+""")
+
+print("\033[1;94------------MENU--------------")
+print("[1]Soal Tentang Pemograman Dasar")
+print("[2]Soal C++")
+print("--------------------------------------")
+pilih = input("Masukan Pilhan anda: ")
+print(" ")
+if pilih == "1":
+	kuis1 = "\033[1;92m 1. Dalam menyusun suatu program,langkah pertama yang harus\ndi lakkukan adalah... "
+	jawaban1 = "a. Membuat Program\nb. Membuat Algoritma\nc. Beli Komputer\nd. Mempelajari Program"
+	print(kuis1)
+	print(jawaban1)
+	hasil = 10
+	respon = input("Masukan jawaban anda a, b, c, d : ")
+	if respon == "b":
+		stop = True
+	else:
+		respon = "a"
+		respon = "c"
+		respon = "d"
+		hasil = 0
+	
+	kuis2 = "\n2. Pseudecode yang digunakan pada penulisan algoritma Berupa... "
+	jawaban2 = "a. Bahasa Inggris\nb. Bahasa Para Bucin\nc. Bahasa Pemograman\nd. Bahasa Zimbabwe"
+	print(kuis2)
+	print(jawaban2)
+	hasil2 = 10
+
+	respon2 = input("Masukan Jawaban anda a, b, c, d : ")
+
+	if respon2 == "c":
+		stop = True
+	else:
+		respon2 = "a"
+		respon2 = "b"
+		respon2 = "d"
+		hasil2 = 0
+
+	kuis3 = "\n3. Pada pembuatan program komputer, algoritma dibuat... "
+	jawaban3 = "a. Sebelum pembuatan program\nb. Pada saat program dibuat\nc. Sesudah membuat program\nd. Pada saat verifikasi"
+	print(kuis3)
+	print(jawaban3)
+	hasil3 = 10
+
+	respon3 = input("Masukan Jawaban anda a, b, c, d : ")
+
+	if respon3 == "a":
+		stop = True
+	else:
+		respon3 = "b"
+		respon3 = "c"
+		respon3 = "d"
+		hasil3 = 0
+
+	kuis4 = "\n4. Sebuah prosedur langkah demi langkah yang pasti untuk\nmenyelesaikan sebuah masalah disebut... "
+	jawaban4 = "a. Proses\nb. Program\nc. Algoritma\nd. Step"
+	print(kuis4)
+	print(jawaban4)
+	hasil4 = 10
+
+	respon4 = input("Masukan jawaban anda a, b, c, d : ")
+
+	if respon4 == "c":
+		stop = True
+	else:
+		respon4 = "a"
+		respon4 = "b"
+		respon5 = "d"
+		hasil4 = 0
+	
+	kuis5 = "\n5. Tahapan dalam menyelesaikan suatu masalah adalah... "
+	jawaban5 = "a. Masalah-Pseudocode-Flowchart-Program-Eksekusi-Hasil\nb. Masalah-Algoritma-Flowchart-Program-Eksekusi-Hasil\nc. Masalah-Model-Algoritma-Eksekusi-Hasil\nd. Masalah-Model-Algoritma-Program-Eksekusi-hasil"
+	print(kuis5)
+	print(jawaban5)
+	hasil5 = 10
+
+	respon5 = input("Masukan jawaban anda a, b, c, d : ")
+
+	if respon5 == "d":
+		stop = True
+	else:
+		respon5 = "a"
+		respon5 = "b"
+		respon5 = "c"
+		hasil5 = 0
+	
+	kuis6 = "\n6. Diberikan algoritma : Apabila warna merah maka jadi hijau\nApabila warna hijau maka jadi putih\nselain warna merah dan hijau maka jadi ungu\nJika kondisi input warna adalah hitam\nmaka warna jadi :"
+	jawaban6 = "a. Merah\nb. Ungu\nc. Hijau\d. Putih"
+	print(kuis6)
+	print(jawaban6)
+	hasil6 = 10
+
+	respon6 = input("Masukan jawaban anda a, b, c, d : ")
+
+	if respon6 == "b":
+		stop = True
+	else:
+		respon6 = "a"
+		respon6 = "c"
+		respon6 = "d"
+		hasil6 = 0
+	
+	kuis7 = "\n7. Instruksi P=Q akan mengakibatkan nilai P=nilaiQ,dan nilai Q\nmenjadi? "
+	jawaban7 = "a. Menjadi sembarang Nilai\nb. Q Tetap\nc. P Tetap\nd. Menjadi Hampa"
+	print(kuis7)
+	print(jawaban7)
+	hasil7 = 10
+
+	respon7 = input("Masukan Jawaban anda a, b, c, d : ")
+
+	if respon7 == "c":
+		stop = True
+	else:
+		respon7 = "a"
+		repson7 = "b"
+		respon7 = "d"
+		hasil7 = 0
+	
+	kuis8 = "\n8. Apabila a=5, b=10, maka jika di berikan instruksi a=b; b=a\nakan mengakibatkan: "
+	jawaban8 = "a. a=0,b=5\nb. a=10,a=10\nc. a=10,a=0\nd. a=b"
+	print(kuis8)
+	print(jawaban8)
+	hasil8 = 10
+
+	respon8 = input("Masukan Jawaban anda a, b, c, d : ")
+
+	if respon8 == "c":
+		stop = True
+	else:
+		respon8 = "a"
+		respon8 = "b"
+		respon8 = "d"
+		hasil8 = 0
+	
+	kuis9 = "\n9. Di berikan algoritma P=10; P=P+5; Q=P. Nilai P dan Q\nmasing-masing adalah..."
+	jawaban9 = "a. 15 dan 0\nb. 0 dan 15\nc. 15 dan 15\nd. 0 dan10"
+	print(kuis9)
+	print(jawaban9)
+	hasil9 = 10
+
+	respon9 = input("Masukan Jawaban anda a, b, c, d : ")
+
+	if respon9 == "c":
+		stop = True
+	else:
+		respon9 = "a"
+		respon9 = "b"
+		respon9 = "d"
+		hasil9 = 0
+
+	kuis10 = "\n10. Apakah fungsi dari tanda // pada bahasa c++ "
+	jawaban10 = "a. Memberikan Syarat pada Program\nb. Memberikan komentar pada program\nc. Memberikan Kritik pada program\nd. Memberikan nilai pada program"
+	print(kuis10)
+	print(jawaban10)
+	hasil10 = 10
+
+	respon10 = input("Masukan jawaban anda a, b, c, d : ")
+
+	if respon10 == "b":
+		stop = True
+	else:
+		respon10 = "a"
+		respon10 = "c"
+		respon10 = "d"
+		hasil10 = 0
+	if (respon, respon2, respon3, respon4, respon5, respon6, respon7, respon8, respon9, respon10):
+		stop = True
+		print("Total Point anda adalah: ", hasil + hasil2 + hasil3 + hasil4 + hasil5 + hasil6 + hasil7 + hasil8 + hasil9 + hasil10)
+	else:
+		print(hasil + hasil2 + hasil3 + hasil4 + hasil5 + hasil6 + hasil7 + hasil8 + hasil9 + hasil10)
+	
+	
+elif pilih == "2":
+	soal = "\033[1;94m  1. Bahasa C++ adalah..."
+	jawab = "a. Sebuah bahasa pemrograman tingkat tinggi\nb. Sebuah bahasa pemograman tingkat atas\nc. Sebuah bahasa pemograman tingkat rendah\nd. Bahasa Bucin"
+	print(soal)
+	print(jawab)
+	point = 10
+
+	res = input("Masukan Jawaban anda a, b, c, d : ")
+	if res == "a":
+		stop = True
+	else:
+		res = "b"
+		res = "c"
+		res = "d"
+		point = 0
+	
+	soal2 = "\n2. Fungsi dari Gets adalah... "
+	jawab2 = "a. Menampilkan Tulisan\nb. Menampilkan Huruf besar\nc. Membaca spasi\nd. Membaca garis bawah"
+	print(soal2)
+	print(jawab2)
+	point2 = 10
+
+	res2 = input("Masukan Jawaban anda a, b, c, d : ")
+	if res2 == "c":
+		stop = True
+	else:
+		res2 = "a"
+		res2 = "b"
+		res2 = "d"
+		point2 = 0
+		
+	soal3 = "\n3. Tipe data berikut akan memberi nilai output, kecuali...."
+	jawab3 = "a. Int\nb. Float\nc. Char\nd. Void"
+	print(soal3)
+	print(jawab3)
+	point3 = 10
+
+	res3 = input("Masukan Jawaban anda a, b, c, d : ")
+	if res3 == "d":
+		stop = True
+	else:
+		res3 = "a"
+		res3 = "b"
+		res3 = "c"
+		point3 = 0
+	
+	soal4 = "\n4. Dibawah ini yang merupakan tipe data dalam c++...."
+	jawab4 = "a. int,float,char,double,conts\nb. float,int,double\nc. int,float,char,double,\nd. conts,main,double"
+	print(soal4)
+	print(jawab4)
+	point4 = 10
+
+	res4 = input("Masukan jawaban anda a, b, c, d : ")
+	if res4 == "c":
+		stop = True
+	else:
+		res4= "a"
+		res4 = "b"
+		res4 = "d"
+		point4 = 0
+		
+	soal5 = "\n5. Penulisan kode program untuk identifier yang benar adalah..."
+	jawab5 = "a. Long 4000;\nb. Float?2;\nc. Long break;\nd. Int,a,b,c;"
+	print(soal5)
+	print(jawab5)
+	point5 = 10
+
+	res5 = input("Masukan Jawaban anda a, b, c, d : ")
+	if res5 == "d":
+		stop = True
+	else:
+		res5 = "a"
+		res5 = "b"
+		res5 = "c"
+		point5 = 0
+	
+	soal6 = "\n6. Apakah fungsi dari frintf?"
+	jawab6 = "a. Menampilkan Variabel\nb. Menampilkan hasil program\nc. Menampilkan Tulisan\nd. Menghapuskan Kalimat"
+	print(soal6)
+	print(jawab6)
+	point6 = 10
+
+	res6 = input("Masukan jawaban anda a, b, c, d : ")
+	if res6 == "c":
+		stop = True
+	else:
+		res6 = "a"
+		res6 = "b"
+		res6 = "d"
+		point6 = 0
+	
+	soal7 = "\n7. Apakah fungsi dari scanf?"
+	jawab7 = "a. Menampilkan hasil input\nb. Menampilkan hasil output\nc. Mengeksekusi Program\d. Menghapus Kalimat"
+	print(soal7)
+	print(jawab7)
+	point7 = 10
+
+	res7 = input("Masukan jawaban anda a, b, c, d : ")
+	if res7 == "a":
+		stop = True
+	else:
+		res7 = "b"
+		res7 = "c"
+		res7 = "d"
+		point7 = 0
+	
+	soal8 = "\n8. Fungsi yang digunakan untuk membuat\nstring menjadi capital adalah..."
+	jawab8 = "a. Strcpy\nb. Strupr\nc. Strcmp\nd. Strcat"
+	print(soal8)
+	print(jawab8)
+	point8 = 10
+
+	res8 = input("Masukan Jawaban anda a, b, c, d : ")
+	if res8 == "b":
+		stop = True
+	else:
+		res8 = "a"
+		res8 = "c"
+		res8 = "d"
+		point8 = 0
+	
+	soal9 = "\n9. Mengapa program bahasa c++ bersifat case sensitive?"
+	jawab9 = "a. Huruf Besar dan kecil sama\nb. Huruf besar dan kecil sama sama\nc. Huruf besar dan kecil berbeda beda tapi tetap satu\nd. Huruf besar dan kecil dianggap beda"
+	print(soal9)
+	print(jawab6)
+	point9 = 10
+
+	res9 = input("Masukan Jawaban anda a, b, c, d : ")
+	if res9 == "d":
+		stop = True
+	else:
+		res9 = "a"
+		res9 = "b"
+		res9 = "c"
+		point9 = 0
+
+	soal10 = "\n10. Perintah yang digunakan pada windows.h yaitu...."
+	jawab10 = "a. Windows.h\nb. iomanip.h\nc. String.sh\nd. stlib.h"
+	print(soal10)
+	print(jawab10)
+	point10 = 10
+
+	res10 = input("Masukan Jawaban anda a, b, c, d : ")
+	if res10 == "a":
+		stop = True
+	else:
+		res10 = "b"
+		res10 = "c"
+		res10 = "d"
+		point10 = 0
+	if (res, res2, res4, res5, res6, res7, res8, res9, res10):
+		stop = True
+		print("Total Point anda adalah : ", point + point2 + point3 + point4 + point5 + point6 + point7 + point8 + point9 + point10)
+	else:
+		print(point + point2 + point3 + point4 + point5 + point6 + point7 + point8 + point9 + point10)
+		
+print("=========================================")
+print("Jika ada kritik dan saran bisa hubungi Author nya:)")
+print("=========================================")
